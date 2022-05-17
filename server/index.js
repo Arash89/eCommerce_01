e2cb286387e2b612c5d6be1784701dc71af17ec1
soudisho <soudisho@adobe.com>
2022-05-17 15:54:15 -0700

    Init commit

---
+++ server/index.js
@@ -0,0 +1,49 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+    type User {
+      id: ID
+      firstName: String
+      age: Int
+      companyName: ID
+    }
+    
+    type Company {
+      id: ID
+      companyName: String
+      description: String
+    }
+    
+    type Query {
+      hello: String
+      users: [User]
+      companies: [Company]
+    }
+    
+    type Mutation {
+      addUser(firstName: String!, age: Int, companyName: ID): User
+    }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: (root, args, context) => "Hello world Arash 6!"
+  },
+  Mutation: {
+    addUser: (root, args, context) => {
+      const { firstName, age, companyName } = args;
+
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
