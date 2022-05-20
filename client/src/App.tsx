350739865e64139df149c55e7eef081f9d0d652f
soudisho <soudisho@adobe.com>
2022-05-20 13:08:40 -0700

    client is working fine

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const HELLO = gql`
+  query Query {
+    hello
+  }
+`
 
 function App() {
+
+  const { data } = useQuery(HELLO)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Application Here: {data.hello}</h3>
     </div>
   );
 }
